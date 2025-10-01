85c2658e1538c29cde65ec28789241a21c438221
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2025-10-01 09:02:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>SFreidArt</title>
+        <link rel="icon" href="/logo25.png" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="SouthernFried Art is Coming Soon!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        Find me on X at @Souther38496623
         </p>
       </main>
 
